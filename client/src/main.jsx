eda010e5aabef4657318767bb68dad3346a35a7f
ameer017 <rajiabdullahi907@gmail.com>
2025-01-09 10:44:53 +0100

    energy context rid of

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,14 +5,12 @@ import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { EnergyContextProvider } from "./context/EnergyContext.jsx";
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <ToastContainer />
-    <EnergyContextProvider>
       <BrowserRouter>
         <App />
       </BrowserRouter>
-    </EnergyContextProvider>
   </StrictMode>
 );
